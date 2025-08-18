f48e3d49eba84c6d8d89cb687098cae9732ada5f
Guillermo Campoy <guillecampoy@gmail.com>
2025-08-18 13:42:10 -0300

    Implemento ejercicio con uso de caracteres de escape Se hace uso de la clase de utilitarios para colorear la salida, se implementra una salida plana como solicita el ejercicio adicionalmente.

---
+++ src/main/java/ejercicio5/CaracteresEscape.java
@@ -0,0 +1,29 @@
+package main.java.ejercicio5;
+
+import main.java.interfaces.Ejecutable;
+import main.java.utils.ContextColor;
+import main.java.utils.UtilsColor;
+
+import java.util.Scanner;
+
+public class CaracteresEscape implements Ejecutable {
+    @Override
+    public void ejecutar() {
+        // Definición de variables
+        String mensaje;
+
+        // Asignación
+        mensaje = "Nombre: Juan Pérez \nEdad: 30 años \nDirección: \"Calle Falsa 123\"";
+
+        //Salida
+        UtilsColor.imprimirEncabezado();
+
+        // Salida formateada
+        UtilsColor.imprimirResultados(ContextColor.INFO, "Salida Formateada");
+        UtilsColor.imprimirResultados(ContextColor.SUCCESS,mensaje);
+        // Salida plana
+        System.out.println();
+        System.out.println("Salida Plana:");
+        System.out.println(mensaje);
+    }
+}
