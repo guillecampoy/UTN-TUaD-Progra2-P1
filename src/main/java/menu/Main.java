1a29ec52504ebcc4c5d2200c2ee7bff0f79570d9
Guillermo Campoy <guillecampoy@gmail.com>
2025-08-18 13:41:44 -0300

    Se refactoriza el nombre del paquete para que sea mas lejible

--- src/main/java/menu/Main.java
+++ src/main/java/menu/Main.java
@@ -1,9 +1,10 @@
-package main.java.main;
+package main.java.menu;
 
 import main.java.ejercicio1.HolaMundo;
 import main.java.ejercicio2.VariablesSimples;
 import main.java.ejercicio3.EntradaUsuario;
 import main.java.ejercicio4.OperacionesAritmeticas;
+import main.java.ejercicio5.CaracteresEscape;
 import main.java.interfaces.Ejecutable;
 
 import java.util.Scanner;
@@ -63,7 +64,7 @@ public class Main {
             case 2 -> new VariablesSimples();
             case 3 -> new EntradaUsuario();
             case 4 -> new OperacionesAritmeticas();
-           // case 5 -> new ejercicio5.CaracteresEscape();
+            case 5 -> new CaracteresEscape();
            // case 6 -> new ejercicio6.ExpresionesInstrucciones();
            // case 7 -> new ejercicio7.ConversionesTipo();
            // case 8 -> new ejercicio8.ErrorEjemploCorregido();
